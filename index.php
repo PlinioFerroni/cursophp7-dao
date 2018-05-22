1969b28a22845607735a83101e64fbbcf6072306
PlinioFerroni <plinio.ferroni@hotmail.com>
2018-05-21 21:40:02 -0300

    criamos a classe usuarios

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php 
 
 require_once("config.php");
-
+/*
 $sql = new sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
+*/
+
+$root = new usuario();
+
+$root->loadById(3);
+
+echo $root;
 
  ?>
